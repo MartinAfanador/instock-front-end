b95bfaac4c39f5e11aa77f8b0f6e3a09ba9aa521
Nicolas7Chaves <nicolas7chaves@gmail.com>
2023-11-08 16:32:11 -0500

    add layout for tablet

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -10,39 +10,50 @@ function ItemDetails() {
                     <div className='item__header-name'> <img className='item__logo' src={backArrow}></img>Item Name</div>
                     <button className='item__header-button'><img className='item__edit' src={edit}></img><div className='item__header-button--tablet'>Edit</div></button>
                 </div>
+
+
                 <div className='item__bottom-container'>
-                    <div className='item__description-container'> {/* item container */}
-                        <div className='item__label'>
-                            Item Description:
-                            <div className='item__description'>
-                                Item description like 50" 4k LED Item description like 50" 4k LED.
+                    <div className='item__container-tablet1'> {/* item container for tablet mode */}
+                        <div className='item__description-container'> {/* item container */}
+                            <div className='item__label'>
+                                Item Description:
+                                <div className='item__description'>
+                                    Item description like 50" 4k LED Item description like 50" 4k LED.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='item__status-container'> {/* status container */}
+                            <div className='item__container-top'>{/* left container */}
+                                <div className='item__label'>
+                                    Category:
+                                    <div className='item__description'>
+                                        Electronics
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div className='item__container-bottom'>
+                        <div className='item__container-tablet2'>
+                            <div className='item__container-bottom-left'>
 
-                    <div className='item__status-container'> {/* status container */}
-                        <div className='item__container-left'>{/* left container */}
-                            <div className='item__label'>
-                                Category:
-                                <div className='item__description'>
-                                    Electronics
+                                <div className='item__label'>
+                                    Status:
+                                    <div className='item__instock'>IN STOCK</div>
+                                </div>
+                                <div className='item__label'>
+                                    Warehouse:
+                                    <div className='item__description'>
+                                        Manhattan
+                                    </div>
                                 </div>
                             </div>
-                            <div className='item__label'>
-                                Status:
-                                <div className='item__instock'>IN STOCK</div>
-                            </div>
-                            <div className='item__label'>
-                                Warehouse:
+                            <div className='item__label item__label--quantity'>
+                                Quantity:
                                 <div className='item__description'>
-                                    Manhattan
+                                    500
                                 </div>
-                            </div>
-                        </div>
-                        <div className='item__label item__label--quantity'>
-                            Quantity:
-                            <div className='item__description'>
-                                500
                             </div>
                         </div>
                     </div>

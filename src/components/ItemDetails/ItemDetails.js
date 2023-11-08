f62a11509924e72768cefd1580f747a3f766bfac
Nicolas7Chaves <nicolas7chaves@gmail.com>
2023-11-08 11:58:22 -0500

    set foundation for item details

---
+++ src/components/ItemDetails/ItemDetails.js
@@ -0,0 +1,15 @@
+import './ItemDetails.scss';
+
+function ItemDetails() {
+    return (
+        <>
+        <div>test
+            <div> Header
+                <div> Item Name</div>
+                <button>Edit</button>
+            </div>
+        </div>
+        </>
+    )
+}
+export default ItemDetails;

b88a362463cd09df3bf1d0e0642732f62eed4b77
Michael Vidal <michaelvidal@Michaels-MacBook-Pro.local>
2023-11-10 00:26:32 -0500

    impliment delete modal

--- src/components/WarehouseList/WList.js
+++ src/components/WarehouseList/WList.js
@@ -1,28 +1,24 @@
-import './WList.scss'
-import { ReactComponent as Delete } from '../../images/delete_black_24dp.svg'
-import { ReactComponent as Edit } from '../../images/edit_black_24dp.svg'
-import { ReactComponent as Chevron } from '../../images/chevron_right_black_24dp.svg'
-import { useState, useEffect } from 'react';
-import axios from "axios";
-
+import './WList.scss';
+import { ReactComponent as Delete } from '../../images/delete_black_24dp.svg';
+import { ReactComponent as Edit } from '../../images/edit_black_24dp.svg';
+import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 import DeleteWarehouse from '../../pages/DeleteWarehouse/DeleteWarehouse';
-import { Link } from 'react-router-dom';
-
 
 function Wlist() {
-    const [data, setData] = useState()
-    // A copy of data list, used for filteration
-    const [searchData, setSearchData] = useState()
+    const [data, setData] = useState([]);
+    const [searchData, setSearchData] = useState([]);
+    const [shouldShowDeleteModal, setShouldShowDeleteModal] = useState(false);
 
-    // Exeecuted once, it will fetch all warehouse list from server
+
+    const itemId = useRef(null);
+
     useEffect(() => {
         async function getWareHouseData() {
             try {
                 const response = await axios.get("http://localhost:8086/api/warehouses");
-                // intially the search data nd server response date are same
                 setData(response.data);
                 setSearchData(response.data);
-                console.log("response.data ", response.data);
             } catch (error) {
                 console.error(error);
             }
@@ -33,85 +29,86 @@ function Wlist() {
     function onSearch(searchText) { }
 
     function showDeleteModal(id) {
-
+        itemId.value = id;
+        // setItemIdToDelete(id);
+        setShouldShowDeleteModal(true);
     }
-    // function onDeleteModalConfirmed() {
-    //     console.log(itemId.value);
 
-    //     const itemIdValue = itemId.value
-    //     const searchItems = searchData.filter(item => item.id !== itemIdValue);
-    //     setSearchData(searchItems);
+    function onDeleteModalCancel() {
+        setShouldShowDeleteModal(false);
+    }
 
-    //     // async function deleteItem() {
-    //     //     try {
-    //     //         const response = await axios.delete(`http://localhost:8086/api/warehouses/${itemId.value}`);
-    //     //         console.log("response.delete ", response.data);
-    //     //     } catch (error) {
-    //     //         console.error(error);
-    //     //     }
-    //     // }
-    //     // deleteItem();
-    //     setShouldShowDeleteModal(false);
-    // }
+    function onDeleteModalConfirmed() {
+        const itemIdToDelete = itemId.value;
 
-    if (data) {
+        async function deleteItem() {
+            try {
+                await axios.delete(`http://localhost:8086/api/warehouses/${itemIdToDelete}`);
+                const filteredData = searchData.filter(item => item.id !== itemIdToDelete);
+                setSearchData(filteredData);
+                setData(filteredData);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setShouldShowDeleteModal(false);
+            }
+        }
+        if (itemIdToDelete) {
+            deleteItem();
+        }
+    }
+
+    let backdrop = null;
+    if (shouldShowDeleteModal) {
+        backdrop = <div className="modal-backdrop" onClick={onDeleteModalCancel}></div>;
+    }
 
 
-        const items = searchData.map(item => {
-            return (
-                <>
-                    <form className='warehouse__list' key={item.id}>
+    let content = null;
+    if (data.length > 0) {
+        const items = searchData.map(item => (
+            <div key={item.id} className='warehouse__list'>
+                <section className='warehouse__list__section'>
+                    <div className='warehouse__list__section-flex'>
+                        <div className='warehouse__list__section-container'>
+                            <div className='warehouse__list__section-label'>WAREHOUSE</div>
+                            <div className='warehouse__list__section-value'>{item.warehouse_name}</div>
+                        </div>
 
-                        <section className='warehouse__list__section'>
-                            <div className='warehouse__list__section-flex'>
-                                <div className='warehouse__list__section-container'>
-                                    <div className='warehouse__list__section-label'>WAREHOUSE</div>
-                                    <div className='warehouse__list__section-value'>{item.warehouse_name}</div>
-                                </div>
+                        <div className='warehouse__list__section-container'>
+                            <div className='warehouse__list__section-label'>ADDRESS</div>
+                            <div className='warehouse__list__section-value'>{item.address}</div>
+                        </div>
+                    </div>
 
-                                <div className='warehouse__list__section-container'>
-                                    <div className='warehouse__list__section-label'>ADDRESS</div>
-                                    <div className='warehouse__list__section-value'>{item.address}</div>
-                                </div>
-                            </div>
+                    <div className='warehouse__list__section-flex'>
+                        <div className='warehouse__list__section-container'>
+                            <div className='warehouse__list__section-label'>CONTACT NAME</div>
+                            <div className='warehouse__list__section-value'>{item.contact_name}</div>
+                        </div>
 
-                            <div className='warehouse__list__section-flex'>
-                                <div className='warehouse__list__section-container'>
-                                    <div className='warehouse__list__section-label'>CONTACT NAME</div>
-                                    <div className='warehouse__list__section-value'>{item.contact_name}</div>
-                                </div>
+                        <div className='warehouse__list__section-container'>
+                            <div className='warehouse__list__section-label'>CONTACT INFORMATION</div>
+                            <div className='warehouse__list__contact-info-phone'>{item.contact_phone}</div>
+                            <div className='warehouse__list__contact-info-email'>{item.contact_email}</div>
+                        </div>
+                    </div>
+                </section>
 
-                                <div className='warehouse__list__section-container'>
-                                    <div className='warehouse__list__section-label'>CONTACT INFORMATION</div>
-                                    <div className='warehouse__list__contact-info-phone'>{item.contact_phone}</div>
-                                    <div className='warehouse__list__contact-info-email'>{item.contact_email}</div>
-                                </div>
-                            </div>
+                <section>
+                    <div className='warehouse__list__actions'>
+                        <Delete className='warehouse__list__actions-delete' onClick={() => showDeleteModal(item.id)} />
+                        <Edit className='warehouse__list__actions-edit' />
+                    </div>
+                </section>
+            </div>
+        ));
 
-                        </section>
-
-                        <section>
-
-                            <div className='warehouse__list__actions'>
-                                <Link to={`/delete-warehouse/${item.id}`}>
-                                    <Delete className='warehouse__list__actions-delete' onClick={() => showDeleteModal(item.id)} />
-                                </Link>
-                                <Edit className='warehouse__list__actions-edit' />
-                            </div>
-                        </section>
-                    </form>
-                    {/* {modal} */}
-                </>
-            );
-        });
-
-        return (
+        content = (
             <div className='warehouse__list'>
-
                 <section className='warehouse__list__header'>
                     <h1 className='warehouse__list__header-title'>Warehouses</h1>
                     <div className='warehouse__list__header-search'>
-                        {/*onChange is called everytime user udpates the search input and user input will be passed to onSearch function  */}
                         <input className='warehouse__list__header-input' placeholder='Search' onChange={(e) => onSearch(e.target.value)} />
                     </div>
                     <div className='warehouse__list__header-add'>
@@ -119,12 +116,20 @@ function Wlist() {
                     </div>
                 </section>
                 {items}
+                {backdrop}
+                <DeleteWarehouse
+                    isOpen={shouldShowDeleteModal}
+                    onCancel={onDeleteModalCancel}
+                    onConfirmed={onDeleteModalConfirmed}
+                />
+
             </div>
         );
     } else {
-        return <div> NO data</div>
+        content = <div>No data</div>;
     }
 
-};
+    return content;
+}
 
-export default Wlist
+export default Wlist;
--- src/pages/DeleteWarehouse/DeleteWarehouse.js
+++ src/pages/DeleteWarehouse/DeleteWarehouse.js
@@ -1,27 +1,26 @@
 import { useParams } from "react-router-dom";
 import "./DeleteWarehouse.scss";
-import { useState } from "react";
 import axios from "axios";
+import { ReactComponent as Close } from '../../images/close_black_24dp.svg';
 
 
-function DeleteWarehouse({ onCancel }) {
+function DeleteWarehouse({ isOpen, onCancel }) {
 
     const { id } = useParams();
 
     function showHome() {
         window.location.href = '/'
-    }    // const [isOpen, setIsOpen] = useState(false)
-
+    }
 
     function onDeleteModalConfirmed() {
         if (!id) return;
-        console.log(id);
+
 
         const itemIdValue = id
         async function deleteItem() {
             try {
                 const response = await axios.delete(`http://localhost:8086/api/warehouses/${id}`);
-                console.log("response.delete ", response.data);
+
                 showHome();
             } catch (error) {
                 console.error(error);
@@ -30,20 +29,25 @@ function DeleteWarehouse({ onCancel }) {
         }
         deleteItem();
     }
-    // if (!isOpen) return null;
+    if (!isOpen) return null;
 
     return (
-        <div className="delete__warehouse">
-            <div className="delete__warehouse-container">
-                <h1 className="delete__warehouse-title">DELETE WASHINGTON WAREHOUSE?</h1>
-                <p className="delete__warehouse-text">LOREM IPSUM</p>
+        <div className="delete__background">
+            <div className="delete__warehouse">
+                <div className="delete__warehouse-box">
+                    <Close className="delete__warehouse-close" onClick={showHome} />
+                </div>
+                <div className="delete__warehouse-container">
+                    <h1 className="delete__warehouse-title">Delete Washington warehouse?</h1>
+                    <p className="delete__warehouse-text">Please confirm that you’d like to delete the Washington from the list of warehouses. You won’t be able to undo this action.</p>
 
-            </div >
-            <div className="delete__warehouse-button-container">
-                <button onClick={showHome}>CANCEL</button>
-                <button onClick={onDeleteModalConfirmed}>DELETE</button>
+                </div >
+                <div className="delete__warehouse__button__container">
+                    <button className='delete__warehouse__button__container-cancel' onClick={onCancel}>Cancel</button>
+                    <button className='delete__warehouse__button__container-delete' onClick={onDeleteModalConfirmed}>Delete</button>
+                </div>
+
             </div>
-
         </div>
     )
 }
